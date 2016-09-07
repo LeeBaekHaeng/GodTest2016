9b04a94b025e4c5a913b3407c359a784ae8f4fcb
LeeBaekHaeng <dlqorgod@naver.com>
2016-09-08 08:14:25 +0900

    역할권한관리, 암호화 복호화 테스트

---
+++ godsoft.com-20160908-crypto/src/test/java/egovframework/com/utl/sim/service/EgovFileScrtyTest.java
@@ -0,0 +1,30 @@
+package egovframework.com.utl.sim.service;
+
+import org.junit.Test;
+
+public class EgovFileScrtyTest {
+
+	@Test
+	public void test() throws Exception {
+		System.out.println("테스트");
+
+		encryptPassword();
+	}
+
+	public void encryptPassword() throws Exception {
+		String password = "rhdxhd12"; // 공통12
+		String id = "id";
+
+		String encryptPassword = EgovFileScrty.encryptPassword(password, id);
+		System.out.println("encryptPassword=" + encryptPassword);
+
+		String encodeBinary = EgovFileScrty.encodeBinary("test".getBytes());
+		System.out.println("encodeBinary=" + encodeBinary);
+
+		byte[] decodeBinary = EgovFileScrty.decodeBinary(encodeBinary);
+		System.out.println("decodeBinary=" + decodeBinary);
+		System.out.println("decodeBinary=" + decodeBinary.toString());
+		System.out.println("decodeBinary=" + new String(decodeBinary));
+	}
+
+}
