a9ee5b8859b0e95b9becc50ba848a7cccb370ba4
이백행 <dlqorgod@naver.com>
2017-03-28 19:24:49 +0900

    테스트20170328

---
+++ kosii.test20170328/src/main/java/kosii/test/curd/comtnemplyrinfo/service/ComtnemplyrinfoVO.java
@@ -0,0 +1,322 @@
+package kosii.test.curd.comtnemplyrinfo.service;
+
+/**
+ * @Class Name : ComtnemplyrinfoVO.java
+ * @Description : Comtnemplyrinfo VO class
+ * @Modification Information
+ *
+ * @author 이백행&lt;dlqorgod@naver.com&gt;
+ * @since 2017-03-28
+ * @version 1.0
+ * @see Copyright (C) All right reserved.
+ */
+public class ComtnemplyrinfoVO extends ComtnemplyrinfoDefaultVO {
+	private static final long serialVersionUID = 1L;
+
+	/** EMPLYR_ID */
+	private java.lang.String emplyrId;
+
+	/** ORGNZT_ID */
+	private java.lang.String orgnztId;
+
+	/** USER_NM */
+	private java.lang.String userNm;
+
+	/** PASSWORD */
+	private java.lang.String password;
+
+	/** EMPL_NO */
+	private java.lang.String emplNo;
+
+	/** IHIDNUM */
+	private java.lang.String ihidnum;
+
+	/** SEXDSTN_CODE */
+	private java.lang.String sexdstnCode;
+
+	/** BRTHDY */
+	private java.lang.String brthdy;
+
+	/** FXNUM */
+	private java.lang.String fxnum;
+
+	/** HOUSE_ADRES */
+	private java.lang.String houseAdres;
+
+	/** PASSWORD_HINT */
+	private java.lang.String passwordHint;
+
+	/** PASSWORD_CNSR */
+	private java.lang.String passwordCnsr;
+
+	/** HOUSE_END_TELNO */
+	private java.lang.String houseEndTelno;
+
+	/** AREA_NO */
+	private java.lang.String areaNo;
+
+	/** DETAIL_ADRES */
+	private java.lang.String detailAdres;
+
+	/** ZIP */
+	private java.lang.String zip;
+
+	/** OFFM_TELNO */
+	private java.lang.String offmTelno;
+
+	/** MBTLNUM */
+	private java.lang.String mbtlnum;
+
+	/** EMAIL_ADRES */
+	private java.lang.String emailAdres;
+
+	/** OFCPS_NM */
+	private java.lang.String ofcpsNm;
+
+	/** HOUSE_MIDDLE_TELNO */
+	private java.lang.String houseMiddleTelno;
+
+	/** GROUP_ID */
+	private java.lang.String groupId;
+
+	/** PSTINST_CODE */
+	private java.lang.String pstinstCode;
+
+	/** EMPLYR_STTUS_CODE */
+	private java.lang.String emplyrSttusCode;
+
+	/** ESNTL_ID */
+	private java.lang.String esntlId;
+
+	/** CRTFC_DN_VALUE */
+	private java.lang.String crtfcDnValue;
+
+	/** SBSCRB_DE */
+	// private java.sql.Date sbscrbDe;
+	private String sbscrbDe;
+
+	public java.lang.String getEmplyrId() {
+		return this.emplyrId;
+	}
+
+	public void setEmplyrId(java.lang.String emplyrId) {
+		this.emplyrId = emplyrId;
+	}
+
+	public java.lang.String getOrgnztId() {
+		return this.orgnztId;
+	}
+
+	public void setOrgnztId(java.lang.String orgnztId) {
+		this.orgnztId = orgnztId;
+	}
+
+	public java.lang.String getUserNm() {
+		return this.userNm;
+	}
+
+	public void setUserNm(java.lang.String userNm) {
+		this.userNm = userNm;
+	}
+
+	public java.lang.String getPassword() {
+		return this.password;
+	}
+
+	public void setPassword(java.lang.String password) {
+		this.password = password;
+	}
+
+	public java.lang.String getEmplNo() {
+		return this.emplNo;
+	}
+
+	public void setEmplNo(java.lang.String emplNo) {
+		this.emplNo = emplNo;
+	}
+
+	public java.lang.String getIhidnum() {
+		return this.ihidnum;
+	}
+
+	public void setIhidnum(java.lang.String ihidnum) {
+		this.ihidnum = ihidnum;
+	}
+
+	public java.lang.String getSexdstnCode() {
+		return this.sexdstnCode;
+	}
+
+	public void setSexdstnCode(java.lang.String sexdstnCode) {
+		this.sexdstnCode = sexdstnCode;
+	}
+
+	public java.lang.String getBrthdy() {
+		return this.brthdy;
+	}
+
+	public void setBrthdy(java.lang.String brthdy) {
+		this.brthdy = brthdy;
+	}
+
+	public java.lang.String getFxnum() {
+		return this.fxnum;
+	}
+
+	public void setFxnum(java.lang.String fxnum) {
+		this.fxnum = fxnum;
+	}
+
+	public java.lang.String getHouseAdres() {
+		return this.houseAdres;
+	}
+
+	public void setHouseAdres(java.lang.String houseAdres) {
+		this.houseAdres = houseAdres;
+	}
+
+	public java.lang.String getPasswordHint() {
+		return this.passwordHint;
+	}
+
+	public void setPasswordHint(java.lang.String passwordHint) {
+		this.passwordHint = passwordHint;
+	}
+
+	public java.lang.String getPasswordCnsr() {
+		return this.passwordCnsr;
+	}
+
+	public void setPasswordCnsr(java.lang.String passwordCnsr) {
+		this.passwordCnsr = passwordCnsr;
+	}
+
+	public java.lang.String getHouseEndTelno() {
+		return this.houseEndTelno;
+	}
+
+	public void setHouseEndTelno(java.lang.String houseEndTelno) {
+		this.houseEndTelno = houseEndTelno;
+	}
+
+	public java.lang.String getAreaNo() {
+		return this.areaNo;
+	}
+
+	public void setAreaNo(java.lang.String areaNo) {
+		this.areaNo = areaNo;
+	}
+
+	public java.lang.String getDetailAdres() {
+		return this.detailAdres;
+	}
+
+	public void setDetailAdres(java.lang.String detailAdres) {
+		this.detailAdres = detailAdres;
+	}
+
+	public java.lang.String getZip() {
+		return this.zip;
+	}
+
+	public void setZip(java.lang.String zip) {
+		this.zip = zip;
+	}
+
+	public java.lang.String getOffmTelno() {
+		return this.offmTelno;
+	}
+
+	public void setOffmTelno(java.lang.String offmTelno) {
+		this.offmTelno = offmTelno;
+	}
+
+	public java.lang.String getMbtlnum() {
+		return this.mbtlnum;
+	}
+
+	public void setMbtlnum(java.lang.String mbtlnum) {
+		this.mbtlnum = mbtlnum;
+	}
+
+	public java.lang.String getEmailAdres() {
+		return this.emailAdres;
+	}
+
+	public void setEmailAdres(java.lang.String emailAdres) {
+		this.emailAdres = emailAdres;
+	}
+
+	public java.lang.String getOfcpsNm() {
+		return this.ofcpsNm;
+	}
+
+	public void setOfcpsNm(java.lang.String ofcpsNm) {
+		this.ofcpsNm = ofcpsNm;
+	}
+
+	public java.lang.String getHouseMiddleTelno() {
+		return this.houseMiddleTelno;
+	}
+
+	public void setHouseMiddleTelno(java.lang.String houseMiddleTelno) {
+		this.houseMiddleTelno = houseMiddleTelno;
+	}
+
+	public java.lang.String getGroupId() {
+		return this.groupId;
+	}
+
+	public void setGroupId(java.lang.String groupId) {
+		this.groupId = groupId;
+	}
+
+	public java.lang.String getPstinstCode() {
+		return this.pstinstCode;
+	}
+
+	public void setPstinstCode(java.lang.String pstinstCode) {
+		this.pstinstCode = pstinstCode;
+	}
+
+	public java.lang.String getEmplyrSttusCode() {
+		return this.emplyrSttusCode;
+	}
+
+	public void setEmplyrSttusCode(java.lang.String emplyrSttusCode) {
+		this.emplyrSttusCode = emplyrSttusCode;
+	}
+
+	public java.lang.String getEsntlId() {
+		return this.esntlId;
+	}
+
+	public void setEsntlId(java.lang.String esntlId) {
+		this.esntlId = esntlId;
+	}
+
+	public java.lang.String getCrtfcDnValue() {
+		return this.crtfcDnValue;
+	}
+
+	public void setCrtfcDnValue(java.lang.String crtfcDnValue) {
+		this.crtfcDnValue = crtfcDnValue;
+	}
+
+	// public java.sql.Date getSbscrbDe() {
+	// return this.sbscrbDe;
+	// }
+	//
+	// public void setSbscrbDe(java.sql.Date sbscrbDe) {
+	// this.sbscrbDe = sbscrbDe;
+	// }
+
+	public String getSbscrbDe() {
+		return this.sbscrbDe;
+	}
+
+	public void setSbscrbDe(String sbscrbDe) {
+		this.sbscrbDe = sbscrbDe;
+	}
+
+}
