5afd701e0f4b381e5e26379fd3fae54d133a234f
이백행 <dlqorgod@naver.com>
2017-09-29 20:32:14 +0900

    오라클 날짜 함수

---
+++ godsoft.sql2017/oracle/ddl/us_com360obj/함수/FN_TO_CHAR_01.sql
@@ -0,0 +1,47 @@
+﻿CREATE OR REPLACE FUNCTION FN_TO_CHAR_01 (
+    IN_CHAR in varchar2
+    , IN_TO_DATE_FMT in varchar2
+    , IN_TO_CHAR_FMT in varchar2
+) RETURN varchar2 IS
+tmpVar varchar2(100);
+/******************************************************************************
+   NAME:       FN_TO_CHAR_01
+   PURPOSE:    
+
+   REVISIONS:
+   Ver        Date        Author           Description
+   ---------  ----------  ---------------  ------------------------------------
+   1.0        2017-09-29   LeeBaekHaeng       1. Created this function.
+
+   NOTES:
+
+   Automatically available Auto Replace Keywords:
+      Object Name:     FN_TO_CHAR_01
+      Sysdate:         2017-09-29
+      Date and Time:   2017-09-29, 오후 6:49:58, and 2017-09-29 오후 6:49:58
+      Username:        LeeBaekHaeng
+      Table Name:       (set in the "New PL/SQL Object" dialog)
+
+******************************************************************************/
+BEGIN
+   tmpVar := IN_CHAR;
+
+select
+    to_char(to_date(IN_CHAR, IN_TO_DATE_FMT), IN_TO_CHAR_FMT)
+    into tmpVar
+from dual
+;
+
+   RETURN tmpVar;
+   EXCEPTION
+--     WHEN NO_DATA_FOUND THEN
+--       NULL;
+     WHEN OTHERS THEN
+       -- Consider logging the error and then re-raise
+--       RAISE;
+   RETURN tmpVar;
+END FN_TO_CHAR_01;
+
+
+
+/
