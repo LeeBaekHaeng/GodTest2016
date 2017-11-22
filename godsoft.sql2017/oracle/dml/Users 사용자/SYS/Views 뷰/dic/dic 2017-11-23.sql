b704755f83ee30e3ca009163b9113d2d1d416edd
이백행 <dlqorgod@naver.com>
2017-11-23 08:07:54 +0900

    sql

---
+++ godsoft.sql2017/oracle/dml/Users 사용자/SYS/Views 뷰/dic/dic 2017-11-23.sql
@@ -0,0 +1,13 @@
+select
+    '    , ' || COLUMN_NAME as select1
+from (
+select
+    ALL_TAB_COLS.*
+from SYS.ALL_TAB_COLS /* 테이블컬럼 */
+where 1 = 1
+    and ALL_TAB_COLS.OWNER = 'SYS'
+    and ALL_TAB_COLS.TABLE_NAME = 'ALL_TAB_COLS'
+order by
+    ALL_TAB_COLS.OWNER, ALL_TAB_COLS.TABLE_NAME, ALL_TAB_COLS.COLUMN_ID
+)
+;
